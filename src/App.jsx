47a4f7017857dd1ac46068c929fb9dfc94909bc2
omar maher <omarmaher1111s@gmail.com>
2026-05-20 19:20:35 +0300

    Search Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
 import './App.css'
-// import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer'
 import './Components/Footer/Footer.css'
-function App() {
-  const [count, setCount] = useState(0)
+import Search from './Components/Search/Search'
 
+function App() {
   return (
-      <>
-    
-      <Footer/>
-      </>  
+    <>
+      <Search />
+      <Footer />
+    </>
   )
 }
 
