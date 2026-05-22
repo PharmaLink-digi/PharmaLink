209ae1233e5b1d7b6c5c50e3caeafaac56a1b022
ali-hatem <ali.h.010900@gmai.com>
2026-05-22 20:44:00 +0300

    routing test

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,32 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
-import SignInForm from "./Components/SignInForm/SignInForm";
-import Home from "./Components/Home/Home";
-import { RouterProvider } from "react-router-dom";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Home from "./Components/Home/Home";
+import Search from "./Components/Search/Search";
+import SignInForm from "./Components/SignInForm/SignInForm";
 
 
 // import SignInForm from './Components/SignInForm/SignInForm'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-     <SignInForm/>
-      {/* <SignInForm/> */}
-      {/* <Home/> */}
-
-      {/* <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} /> 
-      <Route path="/search" element={<Search />} /> */}
-
-      {/* dkkjoiuhgihjldk */}
-
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter> */}
-
-{/* <Route path="/signup" element={<Signup />} />
-<Route path="/بحث" element={<Search />} /> */}
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/signin" element={<SignInForm />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+// Simple 404 Not Found component
+const NotFound = () => (
+  <div style={{ textAlign: "center", marginTop: "2rem" }}>
+    <h2>404 - Page Not Found</h2>
+    <p>The page you are looking for does not exist.</p>
+    <a href="/" style={{ color: "#0f6bff" }}>Go to Home</a>
+  </div>
+);
 
 export default App;
---
+++ src/Components/Signup/Signup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Signup = () => (
+  <div style={{ textAlign: "center", marginTop: "2rem" }}>
+    <h2>Signup Page</h2>
+    <p>This is a placeholder signup page. Implement registration form here.</p>
+    <Link to="/" style={{ color: "#0f6bff" }}>Go to Home</Link>
+  </div>
+);
+
+export default Signup;
